9b548f420a21b5488f7de491b72f58591cc8a925
William Green <williamrgre@gmail.com>
2024-01-26 14:06:07 -0600

    Update transactions.py

--- pages/transactions.py
+++ pages/transactions.py
@@ -1,1 +1,13 @@
+# Transactions page script
+from st_aggrid import AgGrid
+from utils.supabase_ops import fetch_transactions
+import streamlit as st
+
+def transactions_page():
+    st.title('Transactions')
+    transactions_data = fetch_transactions()
+    if not transactions_data.empty:
+        AgGrid(transactions_data)
+    else:
+        st.write("No transactions data to display.")
 
