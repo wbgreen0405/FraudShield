eed1ca3f330d9dd1129dd72e125b80f8ff2e85dc
William Green <williamrgre@gmail.com>
2024-01-26 15:44:47 -0600

    Update supervised_fraud_results.py

--- pages/supervised_fraud_results.py
+++ pages/supervised_fraud_results.py
@@ -1,12 +1,17 @@
 import streamlit as st
+import numpy as np
 import matplotlib.pyplot as plt
-from utils.chart_helpers import plot_confusion_matrix, plot_roc_curve, plot_feature_importances
+from utils.chart_helpers import plot_confusion_matrix, plot_roc_curve
+
+# Assuming `plot_feature_importances` is defined in `utils/chart_helpers`
+from utils.chart_helpers import plot_feature_importances
 
 def supervised_fraud_results_page():
     st.title('Supervised Fraud Model Results')
     
-    if 'y_true' in st.session_state and 'y_pred' in st.session_state and 'y_prob' in st.session_state:
+    if 'rf_model' in st.session_state and 'y_true' in st.session_state:
         # Retrieve the stored data and metrics
+        rf_model = st.session_state['rf_model']
         y_true = st.session_state['y_true']
         y_pred = st.session_state['y_pred']
         y_prob = st.session_state['y_prob']
@@ -16,20 +21,31 @@ def supervised_fraud_results_page():
         st.subheader('Model Metrics')
         st.json(model_metrics)
 
-        # Generate and display charts
+        # Generate and display feature importances chart if the model has the attribute
         st.subheader('Feature Importances')
-        # You need to have the trained model object available here to plot feature importances
-        # model = st.session_state['model']
-        # plot_feature_importances(model)
+        if hasattr(rf_model, 'feature_importances_'):
+            feature_importances = rf_model.feature_importances_
+            indices = np.argsort(feature_importances)[::-1]  # Sort the feature importances in descending order
+            plt.figure()
+            plt.title("Feature Importances")
+            plt.bar(range(len(feature_importances)), feature_importances[indices], align="center")
+            plt.xticks(range(len(feature_importances)), indices)
+            plt.xlim([-1, len(feature_importances)])
+            st.pyplot(plt)
+        else:
+            st.error('Model does not have feature importances.')
 
+        # Generate and display confusion matrix
         st.subheader('Confusion Matrix')
         plot_confusion_matrix(y_true, y_pred)
 
+        # Generate and display ROC curve
         st.subheader('ROC Curve')
         plot_roc_curve(y_true, y_prob)
 
     else:
-        st.error('No prediction data available. Please run predictions first.')
+        st.error('Model and prediction data are not available. Please run predictions first.')
 
 # Run this page function
 supervised_fraud_results_page()
+
