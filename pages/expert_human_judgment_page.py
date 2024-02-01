795698e659d59e6bf28219d739071aa8957d2212
William Green <williamrgre@gmail.com>
2024-02-01 11:29:10 -0600

    Update expert_human_judgment_page.py

--- pages/expert_human_judgment_page.py
+++ pages/expert_human_judgment_page.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import datetime
 import random
-import audit_logs_history_page  # Import the audit_logs_history_page function
+from pages import audit_logs_history_page  # Import the audit_logs_history_page function
 
 # Define an empty list to store audit logs
 audit_logs = []
