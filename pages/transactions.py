bcaafaa930bf587de25a8862ee3cd2001ee44491
William Green <williamrgre@gmail.com>
2024-02-02 18:42:22 -0600

    Update transactions.py

--- pages/transactions.py
+++ pages/transactions.py
@@ -274,17 +274,6 @@ def transactions_page():
     else:
         st.error("No transactions data available.")
 
-def expert_human_judgment_page():
-    st.set_page_config(layout="wide")
-    st.title('Expert Human Judgment')
-
-    # Check if the flag to display the table is set in session_state
-    if hasattr(st.session_state, 'display_combined_flags_table') and st.session_state.display_combined_flags_table:
-        # Display the "Combined Flags and Anomaly Detection Table" here
-        combined_flags_table = st.session_state.get('combined_flags_table', None)
-        if combined_flags_table is not None:
-            st.write("Combined Flags and Anomaly Detection Table:")
-            st.write(combined_flags_table)
 
 if __name__ == '__main__':
     transactions_page()
