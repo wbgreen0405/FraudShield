d3662530b2faa4e2b0cd6240103058cdc6158624
William Green <williamrgre@gmail.com>
2024-02-02 18:44:41 -0600

    Update expert_human_judgment_page.py

--- pages/expert_human_judgment_page.py
+++ pages/expert_human_judgment_page.py
@@ -1,19 +1,20 @@
 import streamlit as st
-import pandas as pd
 
 def expert_human_judgment_page():
-    st.title("Expert Human Judgment")
+    st.set_page_config(layout="wide")
+    st.title('Expert Human Judgment')
 
     # Check if the flag to display the table is set in session_state
     if hasattr(st.session_state, 'display_combined_flags_table') and st.session_state.display_combined_flags_table:
-        # Display the "Combined Flags and Anomaly Detection Table" here
-        combined_flags_table = st.session_state.get('combined_flags_table', None)
+        # Access the "Combined Flags Table" from session_state
+        combined_flags_table = st.session_state.get('combined_flags', None)
         if combined_flags_table is not None:
             st.write("Combined Flags and Anomaly Detection Table:")
-            st.write(combined_flags_table)
+            st.write(combined_flags_table.rename(columns={'model_version': 'model_type', 'prob_score': 'score'}))
     else:
         st.info("No transactions available for expert human judgment.")
 
 if __name__ == '__main__':
     expert_human_judgment_page()
+
 
