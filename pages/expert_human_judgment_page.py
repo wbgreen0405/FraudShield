a985ecc8161220e97865cb32202bd80a66639682
William Green <williamrgre@gmail.com>
2024-02-02 20:54:19 -0600

    Update expert_human_judgment_page.py

--- pages/expert_human_judgment_page.py
+++ pages/expert_human_judgment_page.py
@@ -1,3 +1,10 @@
+import streamlit as st
+import pandas as pd
+import datetime
+import random
+from pages.transactions import log_audit_entry, unified_flags, anomaly_detection_records  # Add these import statements
+
+
 def expert_human_judgment_page():
     st.set_page_config(layout="wide")
     st.title('Expert Human Judgment')
