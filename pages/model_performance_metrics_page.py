2b22b0bd3010447df8995e27f23370fe1ddaae72
William Green <williamrgre@gmail.com>
2024-02-04 19:28:05 -0600

    Delete pages/model_performance_metrics_page.py

--- pages/model_performance_metrics_page.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def model_performance_metrics_page():
-    st.title("Model Performance Metrics")
-    st.write("View precision, recall, F1 score, ROC AUC, etc., and visualize the confusion matrix and ROC curve.")
-    # Visualize model performance metrics
