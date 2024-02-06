53ca97ecab59ed4e81e8fa045806c97d8b1fdb69
William Green <williamrgre@gmail.com>
2024-02-06 10:34:43 -0600

    Update anomoly_detection.py

--- pages/anomoly_detection.py
+++ pages/anomoly_detection.py
@@ -3,22 +3,19 @@ import pandas as pd
 import plotly.express as px
 
 def create_anomaly_detection_plot(analyzed_df):
-    # Normalize LOF scores for visualization
-    analyzed_df['lof_scores_normalized'] = (analyzed_df['lof_scores'] - analyzed_df['lof_scores'].min()) / (analyzed_df['lof_scores'].max() - analyzed_df['lof_scores'].min())
-    
     # Assign 'Outlier Status' based on LOF predictions
     analyzed_df['Outlier Status'] = analyzed_df['lof_predicted_fraud'].map({-1: 'Outlier', 1: 'Inlier'})
 
     # Create scatter plot with 'ref_id' on the x-axis and 'lof_scores_normalized' on the y-axis
     fig = px.scatter(
-        analyzed_df, 
-        x='ref_id', 
-        y='lof_scores_normalized', 
+        analyzed_df,
+        x='ref_id',
+        y='lof_scores_normalized',
         color='Outlier Status',  # Color by Outlier Status for clarity
         title="Anomaly Detection Scatter Plot",
         hover_data=['ref_id', 'lof_scores_normalized']  # Ensure 'lof_scores' is included in your dataframe
     )
-    
+
     # Update layout if needed
     fig.update_layout(xaxis_title="Reference ID", yaxis_title="Normalized LOF Scores")
 
