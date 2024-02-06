8bd94b284f1baf0b10eca063a617fc984f7b555f
William Green <williamrgre@gmail.com>
2024-02-06 06:31:04 -0600

    Update anomoly_detection.py

--- pages/anomoly_detection.py
+++ pages/anomoly_detection.py
@@ -4,32 +4,32 @@ import plotly.graph_objs as go
 
 def create_anomaly_detection_plot(anomaly_df):
     # Normalize LOF scores for visualization
-    anomaly_df['lof_scores_normalized'] = (anomaly_df['lof_scores'] - anomaly_df['lof_scores'].min()) / (anomaly_df['lof_scores'].max() - anomaly_df['lof_scores'].min())
+    analyzed_df['lof_scores_normalized'] = (analyzed_df['lof_scores'] -analyzed_df['lof_scores'].min()) / (analyzed_df['lof_scores'].max() - analyzed_df['lof_scores'].min())
     
     # Assign 'Outlier Status' based on LOF predictions
-    anomaly_df['Outlier Status'] = anomaly_df['lof_predicted_fraud'].map({-1: 'Outlier', 1: 'Inlier'})
+    analyzed_df['Outlier Status'] = analyzed_df['lof_predicted_fraud'].map({-1: 'Outlier', 1: 'Inlier'})
 
     # Create scatter plot
     fig = go.Figure()
 
     # Add inliers
     fig.add_trace(go.Scatter(
-        x=anomaly_df[anomaly_df['Outlier Status'] == 'Inlier']['income'],
-        y=anomaly_df[anomaly_df['Outlier Status'] == 'Inlier']['name_email_similarity'],
+        x=anomaly_df[analyzed_df['Outlier Status'] == 'Inlier']['income'],
+        y=anomaly_df[analyzed_df['Outlier Status'] == 'Inlier']['name_email_similarity'],
         mode='markers',
         marker=dict(size=10, color='blue'),
         name='Inlier',
-        hovertext=anomaly_df[anomaly_df['Outlier Status'] == 'Inlier']['ref_id']
+        hovertext=analyzed_df[analyzed_df['Outlier Status'] == 'Inlier']['ref_id']
     ))
 
     # Add outliers with size representing the LOF score
     fig.add_trace(go.Scatter(
-        x=anomaly_df[anomaly_df['Outlier Status'] == 'Outlier']['income'],
-        y=anomaly_df[anomaly_df['Outlier Status'] == 'Outlier']['name_email_similarity'],
+        x=anomaly_df[analyzed_df['Outlier Status'] == 'Outlier']['income'],
+        y=anomaly_df[analyzed_df['Outlier Status'] == 'Outlier']['name_email_similarity'],
         mode='markers',
-        marker=dict(size=100 * anomaly_df[anomaly_df['Outlier Status'] == 'Outlier']['lof_scores_normalized'], color='red', line=dict(width=2, color='DarkSlateGrey')),
+        marker=dict(size=100 * analyzed_df[analyzed_df['Outlier Status'] == 'Outlier']['lof_scores_normalized'], color='red', line=dict(width=2, color='DarkSlateGrey')),
         name='Outlier',
-        hovertext=anomaly_df[anomaly_df['Outlier Status'] == 'Outlier']['ref_id']
+        hovertext=anomaly_df[analyzed_df['Outlier Status'] == 'Outlier']['ref_id']
     ))
 
     fig.update_layout(title="Anomaly Detection Scatter Plot", xaxis_title="Income", yaxis_title="Name Email Similarity")
