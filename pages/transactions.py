b0c967d2ff527c5274454a6ee98abf8b19f1651e
William Green <williamrgre@gmail.com>
2024-02-07 19:12:53 -0600

    Update transactions.py

--- pages/transactions.py
+++ pages/transactions.py
@@ -145,10 +145,10 @@ def app():
             st.write("### Offline Review Detailed Transactions")
             
             # Filter to include only fraud cases as predicted by RF or LOF
-            #review_df = analyzed_df[(analyzed_df['rf_predicted_fraud'] == 1) | (analyzed_df['lof_predicted_fraud'] == 1)]
+            review_df = analyzed_df[(analyzed_df['rf_predicted_fraud'] == 1) | (analyzed_df['lof_predicted_fraud'] == 1)]
            # review_df = analyzed_df[analyzed_df['Approval Status'] == 'Fraud']
            # review_df = analyzed_df[(analyzed_df['rf_predicted_fraud'] == 1) & (analyzed_df['lof_predicted_fraud'] == 1)]
-            review_df = transactions_df[(transactions_df['rf_predicted_fraud'] == 1) & (transactions_df['lof_predicted_fraud'] == 1)]
+            #review_df = transactions_df[(transactions_df['rf_predicted_fraud'] == 1) & (transactions_df['lof_predicted_fraud'] == 1)]
 
 
 
