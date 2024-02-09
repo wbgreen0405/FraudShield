e10b77cd2cbf8086aea0ca93e70246e5bd80b5ce
William Green <williamrgre@gmail.com>
2024-02-09 11:51:16 -0600

    Update transactions.py

--- pages/transactions.py
+++ pages/transactions.py
@@ -195,8 +195,8 @@ def app():
                 analyzed_df['lof_scores'] = np.nan
             # Prepare Offline Review Detailed Transactions with merged flags
             analyzed_df['Flagged By'] = np.where(analyzed_df['RF Approval Status'] == 'Marked as Fraud', 'RF Model', 
-                                                 np.where(analyzed_df['LOF Status'] == 'Suspected Fraud', 'LOF Model', 'None'))
-            review_df = analyzed_df[(analyzed_df['RF Approval Status'] == 'Marked as Fraud') | (analyzed_df['LOF Status'] == 'Suspected Fraud')]
+                                                 np.where(non_fraud_df['LOF Status'] == 'Suspected Fraud', 'LOF Model', 'None'))
+            review_df = analyzed_df[(analyzed_df['RF Approval Status'] == 'Marked as Fraud') | (non_fraud_df['LOF Status'] == 'Suspected Fraud')]
             cols_order = ['ref_id', 'Flagged By', 'RF Approval Status', 'LOF Status', 'lof_scores', 'rf_prob_scores'] + [col for col in analyzed_df.columns if col not in ['ref_id', 'Flagged By', 'RF Approval Status', 'LOF Status', 'lof_scores', 'rf_prob_scores']]
             review_df = review_df[cols_order]
             st.write("### Offline Review Detailed Transactions")
