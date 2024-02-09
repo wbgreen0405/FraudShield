b87285c5fd52ea97f287d366b29370cd84b9620f
William Green <williamrgre@gmail.com>
2024-02-09 07:05:29 -0600

    Update transactions.py

--- pages/transactions.py
+++ pages/transactions.py
@@ -102,6 +102,9 @@ def perform_inference(transactions_df, rf_model, lof_model):
     # Apply LOF on transactions classified as non-fraud by RF
     non_fraud_df = transactions_df[transactions_df['rf_predicted_fraud'] == 0].copy()
 
+    # Reattach 'ref_id' for merging purposes
+    non_fraud_df['ref_id'] = ref_ids[non_fraud_df.index]
+
     if not non_fraud_df.empty:
         X_lof = non_fraud_df.drop(columns=['fraud_bool', 'rf_predicted_fraud', 'rf_prob_scores', 'RF Approval Status', 'ref_id', 'LOF Status'], errors='ignore')
         lof_predictions = lof_model.fit_predict(X_lof)
