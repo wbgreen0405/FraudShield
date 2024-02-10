55af3edf21711554f6fac724e1dc9cda3c17d3e4
William Green <williamrgre@gmail.com>
2024-02-09 23:06:26 -0600

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,1 +1,9 @@
+from .transactions import app as transactions
+from .approval_system import app as approval_system
+from .anomaly_detection import app as anomaly_detection
+from .case_detail import app as case_detail
+from .test_and_learn_loop_page import app as test_and_learn_loop_page
+from .help_documentation_page import app as help_documentation_page
+from .audit_logs_history_page import app as audit_logs_history_page
+from .supervised_fraud_results_page import app as supervised_fraud_results_page
 
