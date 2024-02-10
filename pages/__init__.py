5ab5b6f1a5793702407fb8d46e09d3be4294c8ed
William Green <williamrgre@gmail.com>
2024-02-09 23:55:24 -0600

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,2 +1,12 @@
-from pages import home, transactions, approval_system, anomaly_detection, case_detail, test_and_learn_loop_page, help_documentation_page, audit_logs_history_page
+# Inside pages/__init__.py
+from .home import app as home_app
+from .transactions import app as transactions_app
+from .approval_system import app as approval_system_app
+from .anomaly_detection import app as anomaly_detection_app
+from .case_detail import app as case_detail_app
+from .test_and_learn_loop_page import app as test_and_learn_loop_page_app
+from .help_documentation_page import app as help_documentation_page_app
+from .audit_logs_history_page import app as audit_logs_history_page_app
+# Add other pages as necessary
+
 
