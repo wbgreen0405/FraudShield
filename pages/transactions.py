46f72576e0f763e190f0fd57c1aec3fe3f6fe553
William Green <williamrgre@gmail.com>
2024-02-10 22:02:46 -0600

    Update transactions.py

--- pages/transactions.py
+++ pages/transactions.py
@@ -246,9 +246,9 @@ def app():
         else:
             st.write("No transactions found.")
 
-if __name__ == '__main__':
-    st.set_page_config(page_title="Transaction Analysis", layout="wide")
-    app()
+#if __name__ == '__main__':
+   # st.set_page_config(page_title="Transaction Analysis", layout="wide")
+app()
 
 
 
