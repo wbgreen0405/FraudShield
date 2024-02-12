f7cb7258546718332d80d4d3d1f72e40dd4cdf4c
William Green <williamrgre@gmail.com>
2024-02-11 22:37:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 # Import page functions from the pages package
-from pages import (
+from src import (
     home_app as home,
     transactions_app as transactions,
     approval_system_app as approval_system,
