e3399f779814483bd1bdd38c8efcf42cf0524ae7
William Green <williamrgre@gmail.com>
2024-02-16 23:29:15 -0600

    Update case_detail.py

--- src/case_detail.py
+++ src/case_detail.py
@@ -98,8 +98,6 @@ def app():
     if 'review_df' in st.session_state and st.session_state['review_df'] is not None:
         review_df = st.session_state['review_df']
 
-        review_df = st.session_state['review_df'].copy()
-
         # Filter for records that are both 'Suspected Fraud' by LOF and 'Marked as Fraud' by RF
         review_df = review_df[(review_df['LOF Status'] == 'Suspected Fraud') & (review_df['RF Approval Status'] == 'Marked as Fraud')]
 
