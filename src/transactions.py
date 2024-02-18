be51c6a8878d59873dcaa47594c737eec2ce087d
William Green <williamrgre@gmail.com>
2024-02-17 20:28:08 -0600

    Update transactions.py

--- src/transactions.py
+++ src/transactions.py
@@ -176,7 +176,10 @@ def app():
             )
 
             #st.session_state['review_df'] = analyzed_df[(analyzed_df['LOF Status'] == 'Suspected Fraud') & (analyzed_df['RF Approval Status'] == 'Marked as Fraud')]
-            st.session_state['review_df']  =analyzed_df.query("`LOF Status` == 'Suspected Fraud' and `RF Approval Status` == 'Marked as Fraud'")
+            st.session_state['review_df']  = analyzed_df
+            mask = np.logical_and(analyzed_df['LOF Status'] == 'Suspected Fraud',analyzed_df['RF Approval Status'] == 'Marked as Fraud')
+            review_df = analyzed_df[mask]
+            #st.session_state['review_df']
 
 
             # Set analysis performed flag
